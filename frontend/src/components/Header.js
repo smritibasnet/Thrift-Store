056f1e9d94ea30c9c29346cc5b0592f8b25cc08c
smb0196 <smb0196@my.londonmet.ac.uk>
2022-02-16 17:12:51 +0545

    add products markup

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,19 +2,20 @@ import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 const Header = () => {
   return (
-    <header>
-      <Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect>
-        <Container>
-          <Navbar.Brand href='/'>Thrift Store</Navbar.Brand>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='mx-auto'>
-              <Nav.Link href='/cart'>Cart</Nav.Link>
-              <Nav.Link href='/login'>Sign In</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+<header>
+<Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
+  <Container>
+    <Navbar.Brand href="/"><i className='fas fa-tshirt'></i>THRIFT STORE</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse className="justify-content-end">
+      <Nav >
+        <Nav.Link href="/cart" ><i className='fas fa-shopping-cart'></i>CART</Nav.Link>
+        <Nav.Link href="/login"><i className='fas fa-user'></i>SIGN IN</Nav.Link>
+        
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
     </header>
   )
 }
---
+++ frontend/src/products.js
@@ -0,0 +1,74 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Asian Indian Pakistani Party Wear',
+    image: '/images/partywear.jpg',
+    description:
+      'Asian Indian Pakistani Party Wear is in good condition and has been worn once by its previous owner ',brand: 'FabIndia',
+    category: 'Party Wear',
+    price: 3999,
+    rating: 4.5,
+    Comments: 12,
+  },
+  {
+    _id: '2',
+    name: 'Yellow Sparkly Body Con',
+    image: '/images/cocktailwear.jpg',
+    description:
+      'Yellow Sparkly Body Con is in best condition and has been worn by Shrinkhala Khatiwada former Miss Nepal in a promotion event',
+    brand: 'Urban Girl',
+    category: 'Cocktail Wear',
+    price: 6999,
+    rating: 4.0,
+    Comments: 8,
+  },
+  {
+    _id: '3',
+    name: 'Maroon One-Piece',
+    image: '/images/maroondress.jpg',
+    description:
+      'Maroon One-Piece is in good condition.',
+    brand: 'Unknown',
+    category: 'One-piece',
+    price: 929.99,
+    rating: 3,
+    Comments: 12,
+  },
+  {
+    _id: '4',
+    name: 'Pink One-piece',
+    image: '/images/pinkdress.jpg',
+    description:
+      'Pink One-piece has never been worn by its previous owner and is brand new',
+    brand: 'Unknown ',
+    category: 'One-piece',
+    price: 3999,
+    rating: 5,
+    Comments: 12,
+  },
+  {
+    _id: '5',
+    name: 'Blue Prom Dress',
+    image: '/images/promdress.jpg',
+    description:'Blue Prom Dress is in its best condition',
+    brand: 'Boutique Item',
+    category: 'Prom Dress',
+    price: 10000,
+    rating: 5,
+    Comments: 10,
+  },
+  {
+    _id: '6',
+    name: 'Light Green Spring Wear',
+    image: '/images/springwear.jpg',
+    description:
+      'Spring Wear is in good condition',
+    brand: 'Unknown',
+    category: 'One-piece',
+    price: 999.99,
+    rating: 4,
+    Comments: 12,
+  },
+]
+
+export default products
