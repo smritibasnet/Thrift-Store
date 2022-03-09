21410ad99413c82d9031f929d7a243ee0b93b4a0
yeksamlahin <nihal@technorio.com>
2022-03-09 22:38:19 +0545

    Fix knex connection

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,12 +1,10 @@
-import knexJs from 'knex'
+import knexJs from "knex";
 
-//import * as knexConfig from './knexfile.js'
-import knexConfig from "./knexfile.js"
+import knexConfig from "./knexfile.js";
 
-console.log(knexConfig)
 /**
  * Database connection.
  */
-const knex = knexJs(knexConfig)
+const knex = knexJs(knexConfig);
 
-export default knex
+export default knex;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,36 +1,36 @@
-import express from 'express'
-import products from './data/products.js'
-import dotenv from 'dotenv'
+import express from "express";
+import products from "./data/products.js";
+import dotenv from "dotenv";
 
-import knex from './database.js'
+import knex from "./database.js";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('API is running...')
-})
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
 
-app.get('/api/users', async (req, res) => {
-  const users = await knex('users').select('*')
-  console.log(users)
+app.get("/api/users", async (req, res) => {
+  const users = await knex("users").select("*");
+  console.log(users);
 
-  res.json(users)
-})
+  res.json(users);
+});
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-)
+);
