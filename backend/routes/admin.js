b5d1afff460e85c10c31a73c1eb9bef5927fcf60
Smriti Basnet <smb0196@my.londonmet.ac.uk>
2022-04-06 18:45:25 +0545

    updated routes

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/admin/add-product', (req, res, next) => {
+router.get('/add-product', (req, res, next) => {
   res.send(
     '<form action="/admin/add-product" method="POST"><input type="text" name="name"><button type="submit"> Add Product</button></form>'
   )
 })
-router.post('/admin/add-product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
   console.log(req.body)
   res.redirect('/')
 })
